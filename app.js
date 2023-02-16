9bb1b002ddcf3d331d84f8d54d78a3f4e6603027
Vitalie Jalbu <40239902+vitalijalbu@users.noreply.github.com>
2023-02-16 22:06:04 +0100

    installed libraries

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+// Require cronjob library
+const nodeCron = require("node-cron");
 // Require the Cloudinary library
 const cloudinary = require('cloudinary').v2
 
+const job = nodeCron.schedule("0 0 0 /1 * *", function jobYouNeedToExecute() {
+    // Do whatever you want in here. Send email, Make  database backup or download data.
+    console.log(new Date().toLocaleString());
+  });
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  res.send('Hello World!')
+    res.send('index')
+})
+app.get('/refresh', (req, res) => {
+    res.send('refresh')
 })
 
 app.listen(port, () => {
