c27dfe29bfb6a4e2704246db46fde613cbdffd6e
Vitalie <40239902+vitalijalbu@users.noreply.github.com>
2023-02-21 18:49:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const port = 3000
 // Require cronjob library
 const nodeCron = require("node-cron");
 // Require the Cloudinary library
-const cloudinary = require('cloudinary').v2
+var cloudinary = require('cloudinary').v2;
 
 // Require the Getty library
 var api = require("gettyimages-api");
 var creds_getty = require('./config/getty');
 var client = new api (creds_getty);
+
+cloudinary.config({
+  cloud_name: 'european-athletics',
+  api_key: '624624683124292',
+  api_secret: '-DJnHDsCmJhCq48g39Cpjz8KNC0'
+});
 
 /*
 const job = nodeCron.schedule("0 0 0 0 * *", function jobYouNeedToExecute() {
@@ -25,8 +31,17 @@ app.get('/', (req, res) => {
         throw err;
     });
 })
-app.get('/refresh', (req, res) => {
-    res.send('refresh')
+
+
+app.post('/upload', (req, res) => {
+  cloudinary.uploader
+  .upload("https://via.placeholder.com/800x533/15202B/FFFFFF?text=gettyimage", {public_id: "/Getty",})
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((error) => {
+    res.send(error);
+  });
 })
 
 app.listen(port, () => {
